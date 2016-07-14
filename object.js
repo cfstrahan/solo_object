63d6b87e97dd484dd1c3f2cf946ac5916126ed90
Cormac <cormac.strahan@gmail.com>
2016-07-14 10:22:14 -0500

    First Commit

---
+++ object.js
@@ -0,0 +1,16 @@
+var mnUnited = {
+  teamName: "Minnesota United FC",
+  league: "NASL",
+  nickname: "Loons",
+  seasonPosition: "Fourth",
+  stadiumLoc: "Midway",
+  stadiumCity: "St. Paul",
+  stadiumDoneYear: "2018",
+  taxDollars: "$0.00",
+  topForward: "Ben Speas",
+  speasNum: "10",
+
+}
+  console.log(mnUnited.teamName + ", occassionally know as the \"" + mnUnited.nickname + ",\" is the current soccer team of Minnesota. The team is currently a part of the " + mnUnited.league + ", but in 2 years will be a part of MLS, the national soccer league. A new stadium for the team will be built in the " + mnUnited.stadiumLoc + " neighborhood in " + mnUnited.stadiumCity + ", and is projected to be done by " + mnUnited.stadiumDoneYear + " and cost about " + mnUnited.taxDollars + " in taxes. The team is currently in " + mnUnited.seasonPosition + " position, with "
+   + mnUnited.topForward + ", number " + mnUnited.speasNum + ", as a leader forward."
+);
